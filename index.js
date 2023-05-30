d8929c13f8c9e3d3afdd976d006071ba5726b6e0
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-05-30 07:55:13 +0500

    Imported Files in index and made connection w db

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const express = require('express');
+const express = require("express");
+
+const { connectToMongoDb } = require("./connection");
+
+const urlRoute = require("./routes/url");
 const app = express();
 const PORT = 8001;
 
+connectToMongoDb("mongodb://localhost:27017/short-url").then(() =>
+  console.log("Connected to MongoDB")
+);
+
+app.use("url/", urlRoute);
 app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
